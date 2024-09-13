e42637e041f4d630fe9eb5a970d4fcbb431e1677
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-13 20:03:38 -0300

    mostrar contagem regressiva

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
 ﻿Console.Clear();
 
-Console.WriteLine("  ||  || ");
-Console.WriteLine("  \\\\()//");
-Console.WriteLine(" //(__)\\\\ ");
-Console.WriteLine(" ||    ||");
+Console.Write("1");
+Thread.Sleep(1000);
+
+Console.Write("2");
+Thread.Sleep(1000);
+
+Console.Write("3");
+Thread.Sleep(1000);
+
+Console.Write("4");
+Thread.Sleep(1000);
+
+Console.Write("5");
+Thread.Sleep(1000);
+
+Console.Write("6");
+Thread.Sleep(1000);
+
+Console.Write("7");
+Thread.Sleep(1000);
+
+Console.Write("8");
+Thread.Sleep(1000);
+
+Console.Write("9");
+Thread.Sleep(1000);
+
+Console.Write("10");
+Thread.Sleep(1000);
