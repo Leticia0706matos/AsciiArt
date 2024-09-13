c7833cbb33400eee66e0d5e54aeac7b5433dd083
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-13 20:28:23 -0300

    mostrar o desenho

--- Program.cs
+++ Program.cs
@@ -1,31 +1,7 @@
-﻿Console.Clear();
+﻿
+Console.Clear();
 
-Console.Write("1");
-Thread.Sleep(1000);
-
-Console.Write("2");
-Thread.Sleep(1000);
-
-Console.Write("3");
-Thread.Sleep(1000);
-
-Console.Write("4");
-Thread.Sleep(1000);
-
-Console.Write("5");
-Thread.Sleep(1000);
-
-Console.Write("6");
-Thread.Sleep(1000);
-
-Console.Write("7");
-Thread.Sleep(1000);
-
-Console.Write("8");
-Thread.Sleep(1000);
-
-Console.Write("9");
-Thread.Sleep(1000);
-
-Console.Write("10");
-Thread.Sleep(1000);
+Console.WriteLine("  ||  || ");
+Console.WriteLine("  \\\\()//");
+Console.WriteLine(" //(__)\\\\ ");
+Console.WriteLine(" ||    ||");
